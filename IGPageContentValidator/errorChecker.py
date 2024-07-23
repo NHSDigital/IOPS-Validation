324de14c4cd8fac06428be96f2de3d414a0e9d3a
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2024-07-23 16:22:44 +0100

    Update errorChecker.py

--- IGPageContentValidator/errorChecker.py
+++ IGPageContentValidator/errorChecker.py
@@ -18,7 +18,21 @@ def FindErrors(url):
             print(url_check)
             for err in error:
                 print(err)
-            print()
+            print("\n")
+        script_tags = soup_check.find_all('script')
+        ''' finds all console.log items, then finds the text associated with it. expect 'console.log(<eror type>,<error var>)' & '<error var> = JSON.stringify(`<text>`). Retuns <error var><text> '''
+        for script in script_tags:
+            script_text = script.get_text()
+            log_messages = re.findall(r'console\.log\((.*?)\)', script_text)
+            for msg in log_messages:
+                msg = (msg.replace("'", "").replace(' ', '').split(','))
+                try:
+                    lines = script_text.splitlines()
+                    for line in lines:
+                        if msg[1] in line and 'var' not in line and 'console.log' not in line:
+                            print(msg[0],line.split("`")[1].rsplit("`", 0)[0])
+                except IndexError:
+                    pass
     print("Check Complete")
     
 
