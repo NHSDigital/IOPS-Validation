c57014882b47962d668668958a283c81c7a48adb
Ryan May <112563297+ryma2fhir@users.noreply.github.com>
2024-08-28 09:19:34 +0100

    Update repoVariables.py

--- QualityControlChecker/repoVariables.py
+++ QualityControlChecker/repoVariables.py
@@ -4,9 +4,9 @@ ukcoreVar = {'project':'UKCore',
              'org':'HL7 UK', 'email':'ukcore@hl7.org.uk'}
 nhseVar = {'project':'England',
            'urlPrefix': 'https://fhir.nhs.uk/England',
-           'ignoreURLPrefix':['http://hl7.org/fhir/5.0','https://digital.nhs.uk/services/organisation-data-service'],
+           'ignoreURLPrefix':['http://hl7.org/fhir/5.0', 'http://hl7.org/fhir/6.0', 'https://digital.nhs.uk/services/organisation-data-service'],
            'org':'NHS England', 
            'email':'interoperabilityteam@nhs.net'}
 nhseProgrammesVar = {'project':'England',
            'urlPrefix': 'https://fhir.nhs.uk/England',
-           'ignoreURLPrefix':['http://hl7.org/fhir/5.0','https://digital.nhs.uk/services/organisation-data-service']}
+           'ignoreURLPrefix':['http://hl7.org/fhir/5.0', 'http://hl7.org/fhir/6.0', 'https://digital.nhs.uk/services/organisation-data-service']}
